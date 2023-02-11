b84beb73d3560e6a1b5080b91943c3dc6187a459
amandaguan-ag <amandaguan1314@gmail.com>
2023-02-11 01:51:00 -0800

    reorganize count function and reorganize tests

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,12 @@
 function count(numInput){
     let numberArray = []
     for (let i = 0; i <= numInput; i ++){
-      if (i.toString().includes("1")){
-        numberArray.push("Beep!");
+      if (i.toString().includes("3")){
+        numberArray.push("Won't you be my neighbor?");
       } else if (i.toString().includes("2")) {
         numberArray.push("Boop!");
-      } else if (i.toString().includes("3")){
-        numberArray.push("Won't you be my neighbor?");
+      } else if (i.toString().includes("1")){
+        numberArray.push("Beep!");
       } else {
         numberArray.push(i);
       }
