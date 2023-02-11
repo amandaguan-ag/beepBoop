8f3d3d5f9f5f5d55a17741e891a87ed669b87e51
amandaguan-ag <amandaguan1314@gmail.com>
2023-02-11 01:22:59 -0800

    add count function,event handler and tdd test

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,30 @@
 // Utility logic
 // business logic
+function count(numInput){
+    let numberArray = []
+    for (let i = 0; i <= numInput; i ++){
+      if (i.toString().includes("3")){
+        numberArray.push("Won't you be my neighbor?");
+      } else if (i.toString().includes("2")) {
+        numberArray.push("Boop!");
+      } else if (i.toString().includes("1")){
+        numberArray.push("Beep!");
+      } else {
+        numberArray.push(i);
+      }
+    };
+    return numberArray.join(", ");
+  }
+  
 // UI logic
-
+window.addEventListener("load", function(){
+    let form = document.querySelector("form")
+    resetBtn.style.display = "none";
+    form.addEventListener("submit", function(event){
+      event.preventDefault();
+      resetBtn.removeAttribute("style");
+      const numInput = document.getElementById("numInput").value;
+      const output = document.getElementById("output")
+      output.append(count(numInput));
+    });
+});
