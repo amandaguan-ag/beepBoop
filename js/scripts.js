bffd12f0bc5b74dccc2a3d72d9f26c19ba437534
amandaguan-ag <amandaguan1314@gmail.com>
2023-02-11 03:34:42 -0800

    trim name input

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,40 @@
 // business logic
-function count(numInput, nameInput){
-    let numberArray = []
-    for (let i = 0; i <= numInput; i ++){
-      if (i.toString().includes("3")){
-        if(nameInput){
+function count(numInput, nameInput) {
+  let numberArray = [];
+  for (let i = 0; i <= numInput; i++) {
+    if (i.toString().includes("3")) {
+      if (nameInput) {
         numberArray.push(`Won't you be my neighbor, ${nameInput.trim()}?`);
-        }else{
-          numberArray.push(`Won't you be my neighbor?`);   
-        }
-      } else if (i.toString().includes("2")) {
-        numberArray.push("Boop!");
-      } else if (i.toString().includes("1")){
-        numberArray.push("Beep!");
       } else {
-        numberArray.push(i);
+        numberArray.push(`Won't you be my neighbor?`);
       }
-    };
-    return numberArray.join(", ");
+    } else if (i.toString().includes("2")) {
+      numberArray.push("Boop!");
+    } else if (i.toString().includes("1")) {
+      numberArray.push("Beep!");
+    } else {
+      numberArray.push(i);
+    }
+  }
+  return numberArray.join(", ");
 }
-  
+
 //UI logic
-window.addEventListener("load", function(){
-    let form = document.querySelector("form")
+window.addEventListener("load", function () {
+  let form = document.querySelector("form");
+  resetBtn.style.display = "none";
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    resetBtn.removeAttribute("style");
+    const numInput = document.getElementById("numInput").value;
+    const nameInput = document.getElementById("name").value;
+    const output = document.getElementById("output");
+    output.append(count(numInput, nameInput));
+  });
+  resetBtn.addEventListener("click", function () {
+    const input = document.querySelector("input");
+    output.innerText = null;
+    input.value = null;
     resetBtn.style.display = "none";
-    form.addEventListener("submit", function(event){
-      event.preventDefault();
-      resetBtn.removeAttribute("style");
-      const numInput = document.getElementById("numInput").value;
-      const nameInput = document.getElementById("name").value;
-      const output = document.getElementById("output")
-      output.append(count(numInput,nameInput));
-    });
-    resetBtn.addEventListener("click", function(){
-        const input = document.querySelector("input");
-        output.innerText = null;
-        input.value = null;
-        resetBtn.style.display = "none";
-      })
+  });
 });
